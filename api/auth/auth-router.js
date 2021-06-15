0424bd610b9afe41bc4a86d412ae69356fff9ddf
devin mitchell <devin.a.mitchell@gmail.com>
2021-06-14 20:13:50 -0600

    MVP done

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -95,7 +95,21 @@ router.post('/login', checkUsernameExists, async (req, res, next) => {
     "message": "no session"
   }
  */
-
+router.get('/logout', (req, res) => {
+  if (req.session.user) {
+    req.session.destroy(err => {
+        if (err) {
+          res.json({ message: 'You cannot leave' })
+        } else {
+          res.json({ message: 'logged out' })
+        }
+    })
+  } else {
+    res.json({
+        message: 'no session'
+    })
+  }
+})
  
 // Don't forget to add the router to the `exports` object so it can be required in other modules
 module.exports = router
